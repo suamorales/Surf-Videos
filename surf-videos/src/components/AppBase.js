44f2e9979623220f92a562b99a5e4a1169bed4e3
suamorales <morales.sua@gmail.com>
2018-08-16 21:11:42 -0400

    Add App container & AppBase components.

---
+++ surf-videos/src/components/AppBase.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AppBase extends Component {
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return(<div> Test </div>);
+  }
+}
+
+export default AppBase;
